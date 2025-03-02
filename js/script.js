854b06de90eb5596c122355d762136eba0171f56
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2025-03-03 02:19:11 +0900

    Create Slide funcsiya

--- js/script.js
+++ js/script.js
@@ -292,11 +292,13 @@ window.addEventListener('DOMContentLoaded', () => {
 			menuEle.innerHTML +=
 
 				`
-				<div>	<img src="${this.src}" alt="${this.alt}">
+				<div>
+				      <img src="${this.src}" alt="${this.alt}">
 							<div>
 								<h3>${this.title} <span class="primary-text">${this.sale}</span></h3>
 								<p>${this.descr}</p>
-							</div></div>
+				      </div>
+				</div>
 			
 				`
 			this.parent.append(menuEle)
@@ -312,6 +314,79 @@ window.addEventListener('DOMContentLoaded', () => {
 			new Ourmenu(src,alt,title,sale,descr,'.menu-items',).renderMenu()
 		})
 		
+	})
+
+	// SLIDER
+	const slides = document.querySelectorAll(".offer__slide"),
+		prev = document.querySelector(".offer__slider-prev"),
+		next = document.querySelector(".offer__slider-next"),
+		total = document.querySelector("#total"),
+		current = document.querySelector("#current"),
+		slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+		slidesInner = document.querySelector(".offer__slider-inner"),
+		width = window.getComputedStyle(slidesWrapper).width
+
+	let slideIndex = 1,
+		offset = 0
+
+	if(slides.length < 10) {
+		total.textContent = `0${slides.length}`
+		current.textContent = `0${slideIndex}`
+	}else {
+		total.textContent = slides.length
+		current.textContent = slideIndex
+	}
+
+	slidesInner.style.width = 100 * slides.length + "%"
+	slidesInner.style.display = "flex"
+	slidesInner.style.transition = "all .5s ease"
+
+	slidesWrapper.style.overflow = "hidden"
+
+	slides.forEach(slide => {
+		slide.style.width = width
+	})
+
+	next.addEventListener("click", () => {
+		if(offset === +width.slice(0, width.length - 2) * (slides.length - 1)) { 
+			offset = 0
+		} else {
+			offset += +width.slice(0, width.length - 2)
+		}
+		slidesInner.style.transform = `translateX(-${offset}px)`
+
+		if(slideIndex === slides.length) {
+			slideIndex = 1
+		}else {
+			slideIndex++
+		}
+
+		if(slides.length < 10) {
+			current.textContent = `0${slideIndex}`
+		} else {
+			current.textContent = slideIndex
+		}
+	})
+
+	prev.addEventListener("click", () => {
+		if(offset === 0) { 
+			offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+		} else {
+			offset -= +width.slice(0, width.length - 2)
+		}
+		slidesInner.style.transform = `translateX(-${offset}px)`
+
+		if(slideIndex === 1) {
+			slideIndex = slides.length
+		}else {
+			slideIndex--
+		}
+
+		if(slides.length < 10) {
+			current.textContent = `0${slideIndex}`
+		} else {
+			current.textContent = slideIndex
+		}
 	})
 
 })
